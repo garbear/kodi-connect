714d65b08478a6b40bedd7570f61a3b35b49b189
Garrett Brown <themagnificentmrb@gmail.com>
2022-02-03 21:01:39 -0800

    Add Mocha/Chai test framework
    Mocha's TypeScript support is poor. In the past I've used ts-mocha,
    which is a thin TypeScript wrapper around Mocha, but that doesn't
    handle ES6 modules, and I want to stay away from CommonJS.
    
    To solve this, Babel is used to compile TypeScript into JavaScript
    for Mocha at runtime.

---
+++ test/test-mocha.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2021 Aclima, Inc.
+ * This file is part of iot2web - https://github.com/Aclima/iot2web
+ */
+
+/* eslint @typescript-eslint/no-unused-expressions: "off" */
+
+import chai from "chai";
+
+describe("Mocha", function () {
+  it("should perform a basic test", async function () {
+    chai.expect(true).to.be.true;
+  });
+});
