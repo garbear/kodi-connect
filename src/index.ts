89de3af05a8c3adab3ed51e891a8d4e005b3c55f
Garrett Brown <themagnificentmrb@gmail.com>
2022-02-03 21:01:34 -0800

    Add Yarn packaging for Typescript project

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2022 Team Kodi
+ * This file is part of kodi-connect - https://github.com/kodi-game/kodi-connect
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ * See LICENSE.txt for more information.
+ */
